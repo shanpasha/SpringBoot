3aba51b2daae647dfd400a2271f2b4feef53d943
new <new@shan>
2023-02-18 18:56:15 +0530

    SpringBoot Actuatot Client

--- 015_SpringActuator/src/main/java/com/controller/StudentController.java
+++ 015_SpringActuator/src/main/java/com/controller/StudentController.java
@@ -1,6 +1,6 @@
 package com.controller;
 
-
+//rest
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
